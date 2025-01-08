a534b236b13d58a3708b8bf4da75260a54bf5875
Pony <74635349+Epic-Deno@users.noreply.github.com>
2025-01-08 16:33:17 +0800

    changes

--- reptile/demo_01.py
+++ reptile/demo_01.py
@@ -3,7 +3,7 @@ Description: 爬测试页面
 Author: zhang zhen
 Date: 2025-01-08 16:26:29
 LastEditors: zhang zhen
-LastEditTime: 2025-01-08 16:31:08
+LastEditTime: 2025-01-08 16:33:09
 FilePath: /PythonLearning2025/reptile/demo_01.py
 '''
 import requests
@@ -11,5 +11,6 @@ import requests
 url = "http://127.0.0.1:3000"
 
 dic = {
+    "accept-language": "ja,zh;q=0.9",
     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"
 }
