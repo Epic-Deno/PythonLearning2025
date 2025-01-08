7a1e1b34e8e72bab0e8f49052ea73f1489613eb4
Pony <74635349+Epic-Deno@users.noreply.github.com>
2025-01-08 16:34:12 +0800

    changes

--- reptile/demo_01.py
+++ reptile/demo_01.py
@@ -14,3 +14,5 @@ dic = {
     "accept-language": "ja,zh;q=0.9",
     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"
 }
+
+resp = requests.get(url, headers = dic) # 处理头部
