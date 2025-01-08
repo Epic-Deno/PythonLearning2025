df0ce62dc8613349ca1fe04d0039211112399da3
Pony <74635349+Epic-Deno@users.noreply.github.com>
2025-01-08 16:31:18 +0800

    changes

---
+++ reptile/demo_01.py
@@ -0,0 +1,11 @@
+'''
+Description: 爬测试页面
+Author: zhang zhen
+Date: 2025-01-08 16:26:29
+LastEditors: zhang zhen
+LastEditTime: 2025-01-08 16:31:08
+FilePath: /PythonLearning2025/reptile/demo_01.py
+'''
+import requests
+
+url = "http://127.0.0.1:3000"
