1f7ffc4b4048cdaf16118bff728581058f550b15
Pony <74635349+Epic-Deno@users.noreply.github.com>
2025-01-09 18:09:22 +0800

    对接

---
+++ reptile/5_HTTP协议.py
@@ -0,0 +1,16 @@
+'''
+Description: main description
+Author: zhang zhen
+Date: 2025-01-09 18:09:16
+LastEditors: zhang zhen
+LastEditTime: 2025-01-09 18:09:17
+FilePath: /PythonLearning2025/reptile/5_HTTP协议.py
+'''
+'''
+Description: 5_HTTP协议
+Author: Pony
+Date: 2021-10-05 09:24:35
+LastEditors: Pony
+LastEditTime: 2021-10-05 09:24:35
+FilePath: /爬虫/第一章/5_HTTP协议.py
+'''
