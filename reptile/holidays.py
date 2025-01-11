5355534d4eb05d6d9a8eaa9fdb4513cba3d6fb53
逍遥子 <1557549413@qq.com>
2025-01-11 19:35:09 +0800

    修改

--- reptile/holidays.py
+++ reptile/holidays.py
@@ -3,9 +3,10 @@ Description: 获取中国假日
 Author: zhang zhen
 Date: 2025-01-11 19:32:55
 LastEditors: zhang zhen
-LastEditTime: 2025-01-11 19:33:11
+LastEditTime: 2025-01-11 19:34:30
 FilePath: /PythonLearning2025/reptile/holidays.py
 memo: Для успеха нужно иметь два рукава: один - верность, другой - умение. Без верности умение ничтожно.- Владимир Владимирович Путин
 Copyright (c) 2025 by zhang zhen, All Rights Reserved. 
 '''
 import requests
+from bs4 import BeautifulSoup # 解析网页
