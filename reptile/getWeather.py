665c8c305ba2afc49da8215e9922f0c9961fc6fa
Pony <74635349+Epic-Deno@users.noreply.github.com>
2025-01-13 11:03:51 +0800

    地址

--- reptile/getWeather.py
+++ reptile/getWeather.py
@@ -3,8 +3,11 @@ Description: 获取天气的脚本
 Author: zhang zhen
 Date: 2025-01-13 11:00:48
 LastEditors: zhang zhen
-LastEditTime: 2025-01-13 11:00:49
+LastEditTime: 2025-01-13 11:03:41
 FilePath: /PythonLearning2025/reptile/getWeather.py
 '''
 import requests
 from bs4 import BeautifulSoup # 解析网页
+
+def getWeather(): # 获取城市天气
+    url = "https://www.weather.com.cn/weather/101190101.shtml" # 南京天气地址
