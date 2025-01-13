4b4a4410ec00f631e1f752e08dc4742d898c6e26
Pony <74635349+Epic-Deno@users.noreply.github.com>
2025-01-13 11:08:24 +0800

    修改

--- reptile/getWeather.py
+++ reptile/getWeather.py
@@ -3,13 +3,16 @@ Description: 获取天气的脚本
 Author: zhang zhen
 Date: 2025-01-13 11:00:48
 LastEditors: zhang zhen
-LastEditTime: 2025-01-13 11:05:27
+LastEditTime: 2025-01-13 11:07:41
 FilePath: /PythonLearning2025/reptile/getWeather.py
 '''
 import requests
-from bs4 import BeautifulSoup # 解析网页
+# from bs4 import BeautifulSoup # 解析网页
 
 def getWeather(): # 获取城市天气
     url = "https://www.weather.com.cn/weather/101190101.shtml" # 南京天气地址
     rep = requests.get(url) # 获取网页内容
     rep.encoding = 'utf-8' # 固定编码格式
+    print(rep.text)
+
+getWeather()
